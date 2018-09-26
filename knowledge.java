10e0ed08543fec3a437971e78901d35dbbae9dc7
sincs99 <43533201+sincs99@users.noreply.github.com>
2018-09-26 13:15:53 +0200

    Create knowledge.java

---
+++ knowledge.java
@@ -0,0 +1,14 @@
+/*
+  knowledge.java 
+	Testprogramm Aufgabenblatt 1, Aufgabe 3
+*/
+public class knowledge {
+
+/**
+* Gibt ein Zitat aus auf mehrere Linien verteilt
+*/
+public static void main(String[] args) {
+System.out.println("Knowledge \nis \nPower");
+}
+
+}
